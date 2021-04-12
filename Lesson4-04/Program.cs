6464b80ba764195faaca6584f816dcb0d3d199b8
Kuklina <kuklina.aleksandra@rambler.ru>
2021-04-12 20:24:17 +0500

    Добавьте файлы проекта.

---
+++ Lesson4-04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson4_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Число Фибоначи " + Fib(n));
+            Console.WriteLine();
+        }
+        static int Fib(int n)
+        {
+            return Fib(n, out _);
+        }
+        static int Fib(int n, out int f)
+        {
+            f = 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                if (n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    int f1;
+                    f = Fib(n - 1, out f1);
+                    return f + f1;
+                }
+            }
+        }
+    }
+}
